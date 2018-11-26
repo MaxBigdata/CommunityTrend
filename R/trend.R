bbb49743606e1c0168ff49532b811075ced3a099
Max <familybjp@163.com>
2018-11-26 16:48:16 +0800

    improvement1

--- R/trend.R
+++ R/trend.R
@@ -31,9 +31,11 @@ trend <- function(community_name_x) {
 
   fileRoute<-system.file("extdata","community_attributes.csv",package = "CommunityTrend")
   fileRoute2<-system.file("extdata","community_hit_attributes.csv",package = "CommunityTrend")
+  fileRoute3<-system.file("extdata","improvement1_attributes.csv",package = "CommunityTrend")
 
   community_attributes<-read_csv(fileRoute)
   community_hit_attributes<-read_csv(fileRoute2)
+  community_up_and_transaction_attributes<-read_csv(fileRoute3)
 
   the_attributes<-community_attributes%>%
     filter(community_name == community_name_x)
@@ -41,13 +43,24 @@ trend <- function(community_name_x) {
   the_attributes2<-community_hit_attributes%>%
     filter(community_name == community_name_x)
 
+  the_attributes3<-ommunity_up_and_transaction_attributes%>%
+    filter(community_name == community_name_x)
+
   print('-------------------------------------')
   print('|过去一个月内自如:')
+  print(paste0("|商圈总房数：",the_attributes3$all_num_business_district))
+  print(paste0("|社区总房数：",the_attributes3$all_num_community))
   print(paste0("|社区合租单间上架：",the_attributes2$community_other_num))
   print(paste0("|平均展览时间:",the_attributes2$average_exhibition_period))
-  print(paste0("|成为火房比例:",the_attributes2$HuoFang_rate))
+  #print(paste0("|成为火房比例:",the_attributes2$HuoFang_rate))
   print(paste0("|调价房数量:",the_attributes2$price_adjustment_num))
   print(paste0("|调价房平均降价:",the_attributes2$price_adjustment_average))
+  print('|')
+  print(paste0("|社区月内新上架单间成交量:",the_attributes3$community_sales_num))
+  print(paste0("|社区月内新上架单间平均展示期:",the_attributes3$exbination_and_adjustment_ave_exhibition_period))
+  print(paste0("|社区月内新上架单间平均调价数:",the_attributes3$price_adjustment_num))
+  print(paste0("|社区月内新上架单间平均调价价格:",the_attributes3$price_adjustment_average))
+
   print('|')
   print('|')
   print('|--------：成交数据||上架数据')
@@ -56,13 +69,11 @@ trend <- function(community_name_x) {
   print(paste0("|社区合租单间销量：",the_attributes$community_other_num,"||",the_attributes2$community_other_num))
   print(paste0("|社区合租单间均价：",the_attributes$community_average_price,"||",the_attributes2$community_average_price))
   print(paste0("|社区合租单间平均面积：",the_attributes$community_average_area,"||",the_attributes2$community_average_area))
-  print(paste0("|社区合租单间平均楼层：",the_attributes$community_average_floor,"||",the_attributes2$community_average_floor))
   print(paste0("|社区合租单位面积价格：",the_attributes$community_average_AreaPrice,"||",the_attributes2$community_average_AreaPrice))
   print('|')
   print(paste0("|当地商圈合租单间销量：",the_attributes$business_district_other_num,"||",the_attributes2$business_district_other_num))
   print(paste0("|当地商圈合租单间均价：",the_attributes$business_district_average_price,"||",the_attributes2$business_district_average_price))
   print(paste0("|当地商圈合租单间平均面积：",the_attributes$business_district_average_area,"||",the_attributes2$business_district_average_area))
-  print(paste0("|当地商圈合租单间平均楼层：",the_attributes$business_district_average_floor,"||",the_attributes2$business_district_average_floor))
   print(paste0("|当地商圈合租单间单位面积价格：",the_attributes$business_district_average_AreaPrice,"||",the_attributes2$business_district_average_AreaPrice))
   print('-------------------------------------')
   #return(the_attributes)
